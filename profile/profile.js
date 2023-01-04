1154bf90220176cdfe541165176a2d95cfc0fde8
Daniel Kuhn <dkuhn@pgh.yearup.org>
2023-01-04 08:50:47 -0500

    add js clean up and more animation, add user detection, start work on profile posts.

--- profile/profile.js
+++ profile/profile.js
@@ -7,8 +7,22 @@ function postPost(){
 }
 username="Slagathor"
 function getUsername(){
-    for (let i = 0; i < 2; i++) {
     document.getElementById("username").innerHTML = username;
-    }
 }
 window.onload=getUsername()
+
+let loginData = JSON.parse(window.localStorage.getItem("login-data"))
+console.log(`login token ${loginData}`)
+fetch('https://microbloglite.herokuapp.com/api/posts', {
+    method: "POST",
+    headers: {authorization: `bearing ${loginData.token}`}
+})
+
+userPosts()
+function userPosts(){
+   let shownPosts = document.getElementById('shownPosts')
+   for (let i = 0; i < 5; i++) {
+   let showingPosts = document.createElement('th')
+   shownPosts.appendChild(showingPosts).innerText = "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.";
+   }
+}
